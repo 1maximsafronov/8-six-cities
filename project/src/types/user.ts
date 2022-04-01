352eb60afc2cc92aba898ddb03dd31efba6b97d1
Maxim Safronov <1maximsafronov@gmail.com>
2022-04-01 15:46:34 +0700

    Добавляет описание типов данных пользователя

---
+++ project/src/types/user.ts
@@ -0,0 +1,14 @@
+
+export type LoginData = {
+  email: string;
+  password: string;
+};
+
+export type AuthData = {
+  id: number;
+  email: string;
+  name: string;
+  avatarUrl: string;
+  isPro: boolean;
+  token: string;
+};
