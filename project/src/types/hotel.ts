0569177d3dfa287a0973d141c65664baee1f09ea
Maxim Safronov <1maximsafronov@gmail.com>
2022-04-01 15:42:15 +0700

    Добавляет описание типа комментария

--- project/src/types/hotel.ts
+++ project/src/types/hotel.ts
@@ -2,19 +2,19 @@ export type Location = {
   latitude: number;
   longitude: number;
   zoom: number;
-}
+};
 
 export type City = {
   name: string;
   location: Location;
-}
+};
 
 export type Host = {
   id: number | string;
   name: string;
   isPro: boolean;
   avatarUrl: string;
-}
+};
 
 export type Hotel = {
   id: number | string;
@@ -33,6 +33,6 @@ export type Hotel = {
   city: City;
   host: Host;
   location: Location;
-}
+};
 
 export type Hotels = Hotel[];
