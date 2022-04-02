4c8c72805e5891e20a38e2e2d8b1f82f7d5ff4f5
Maxim Safronov <1maximsafronov@gmail.com>
2022-04-02 17:59:14 +0700

    Шаблонизирует FavoritesCard

--- project/src/components/blocks/favorites/favorites-card/favorites-card.tsx
+++ project/src/components/blocks/favorites/favorites-card/favorites-card.tsx
@@ -1,15 +1,29 @@
+import classNames from 'classnames';
+import { Link } from 'react-router-dom';
+
 function FavoritesCard():JSX.Element {
 
+  const id = 1;
   const price = 180;
   const title = 'Nice, cozy, warm big bed apartment';
   const type = 'Apartment';
+  const previewImage = 'img/apartment-small-03.jpg';
+  const rating = 5;
+  const isFavorite = false;
+
+  const percent = rating * 20;
+
+  const bookmarClassName = classNames(
+    'place-card__bookmark-button button',
+    {'place-card__bookmark-button--active': isFavorite},
+  );
 
   return (
     <article className="favorites__card place-card">
       <div className="favorites__image-wrapper place-card__image-wrapper">
-        <a href="#">
-          <img className="place-card__image" src="img/apartment-small-03.jpg" width="150" height="110" alt="Place image" />
-        </a>
+        <Link to={`/offer/${id}`}>
+          <img className="place-card__image" src={previewImage} width="150" height="110" alt={`Place ${title}`} />
+        </Link>
       </div>
       <div className="favorites__card-info place-card__info">
         <div className="place-card__price-wrapper">
@@ -17,7 +31,7 @@ function FavoritesCard():JSX.Element {
             <b className="place-card__price-value">&euro;{price}</b>
             <span className="place-card__price-text">&#47;&nbsp;night</span>
           </div>
-          <button className="place-card__bookmark-button place-card__bookmark-button--active button" type="button">
+          <button className={bookmarClassName} type="button">
             <svg className="place-card__bookmark-icon" width="18" height="19">
               <use xlinkHref="#icon-bookmark"></use>
             </svg>
@@ -26,12 +40,12 @@ function FavoritesCard():JSX.Element {
         </div>
         <div className="place-card__rating rating">
           <div className="place-card__stars rating__stars">
-            <span style={{width: '100%'}}></span>
+            <span style={{width: `${percent}%`}}></span>
             <span className="visually-hidden">Rating</span>
           </div>
         </div>
         <h2 className="place-card__name">
-          <a href="#">{title}</a>
+          <Link to={`/offer/${id}`}>{title}</Link>
         </h2>
         <p className="place-card__type">{type}</p>
       </div>
--- project/src/components/blocks/favorites/favorites-locations-items/favorites-locations-items.tsx
+++ project/src/components/blocks/favorites/favorites-locations-items/favorites-locations-items.tsx
@@ -1,12 +1,14 @@
 import FavoritesPlaces from '../favorites-places/favorites-places';
 
 function FavoritesLocationsItems():JSX.Element {
+  const location = 'Amsterdam';
+
   return (
     <li className="favorites__locations-items">
       <div className="favorites__locations locations locations--current">
         <div className="locations__item">
           <a className="locations__item-link" href="#">
-            <span>Amsterdam</span>
+            <span>{location}</span>
           </a>
         </div>
       </div>
